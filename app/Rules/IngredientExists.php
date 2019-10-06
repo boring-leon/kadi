61d2beec9937646850aaef561efdb69279115431
LeonCzerwinski <poriusz@wp.pl>
2019-10-06 14:29:47 +0200

    Created isCustomIngredient() which works even if a given ingredient has no is_custom key

--- app/Rules/IngredientExists.php
+++ app/Rules/IngredientExists.php
@@ -9,13 +9,16 @@ use Illuminate\Contracts\Validation\Rule;
 class IngredientExists implements Rule
 {
     public function passes($attribute, $value){
-       return $value['is_custom'] ? 
+       return $this->isCustomIngredient($value) ? 
        CustomIngredient::where(['id' => $value['id'], 'user_id' => request('user_id')])->exists() : 
        Ingredient::find($value['id']);
     }
 
-    public function message()
-    {
+    public function message(){
         return 'Podany składnik nie istnieje';
+    }
+
+    private function isCustomIngredient(array $data) : bool {
+        return array_key_exists('is_custom', $data) ? $data['is_custom'] : false;
     }
 }
