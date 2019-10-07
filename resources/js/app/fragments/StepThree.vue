9f4a4e0e1ba470b8d27837770babb060f6326efd
LeonCzerwinski <poriusz@wp.pl>
2019-10-07 19:58:40 +0200

    No longer setting kcal or exchangers on store Plate ingredients directly, but on a mapped copy array

--- resources/js/app/fragments/StepThree.vue
+++ resources/js/app/fragments/StepThree.vue
@@ -1,42 +1,49 @@
 <template>
   <article class="text-center">
     <HeaderCenter>Podsumowanie</HeaderCenter>
-      
-    <SummaryTable :ingredients="ingredients" :totalExchangers="totalExchangers" :totalKcal='totalKcal' />
-    <hr>
+
+    <SummaryTable
+      :ingredients="ingredients"
+      :totalExchangers="totalExchangers"
+      :totalKcal="totalKcal"
+    />
+    <hr />
     <section>
       <ExchangerModal id="exchanger_modal" />
-      <ActivitiesModal id="activities_modal" :kcal ='totalKcal' />
+      <ActivitiesModal id="activities_modal" :kcal="totalKcal" />
       <CreateMeal id="meal_modal" />
 
-      <div class="alert alert-info total-units" style='margin-bottom:5;'>
+      <div class="alert alert-info total-units" style="margin-bottom:5;">
         <h4 style="margin:0; padding:0;">Sugerowana ilość jednostek {{ totalUnits | round(2) }}</h4>
       </div>
-      <div class="alert alert-danger" style='margin-bottom:40px;'>
-        <span style='font-size:1.1rem;'>
-          Sprawdź przelicznik - <b> może być inny w zależności od pory dnia </b> !
+      <div class="alert alert-danger" style="margin-bottom:40px;">
+        <span style="font-size:1.1rem;">
+          Sprawdź przelicznik -
+          <b>może być inny w zależności od pory dnia</b> !
         </span>
       </div>
     </section>
-    <hr>
+    <hr />
     <footer>
       <div class="absolute-bottom btn-block-container">
-        <button class="btn btn-block btn-lg btn-danger mb-2" @click="displayModal('exchanger_modal')">
-          Zmień przelicznik J/WW ({{ userExchanger }})
-        </button>
-        <button class='btn btn-info btn-lg btn-block' @click="displayModal('activities_modal')">
-          Jak to spalić?
-        </button>
-        <button class='btn btn-success btn-lg btn-block' @click="displayModal('meal_modal')">
-          Zapisz danie
-        </button>
+        <button
+          class="btn btn-block btn-lg btn-danger mb-2"
+          @click="displayModal('exchanger_modal')"
+        >Zmień przelicznik J/WW ({{ userExchanger }})</button>
+        <button
+          class="btn btn-info btn-lg btn-block"
+          @click="displayModal('activities_modal')"
+        >Jak to spalić?</button>
+        <button
+          class="btn btn-success btn-lg btn-block"
+          @click="displayModal('meal_modal')"
+        >Zapisz danie</button>
       </div>
     </footer>
   </article>
 </template>
 
 <script>
-import { mapState } from "vuex";  
 import SummaryTable from "../components/Summary/SummaryTable.vue";
 import ExchangerModal from "../components/Summary/ExchangerModal.vue";
 import ActivitiesModal from "../components/Summary/ActivitiesModal.vue";
@@ -45,23 +52,14 @@ import CreateMeal from "../components/CustomMeal/Controllers/Create.vue";
 export default {
   name: "StepThreeFragment",
   components: { SummaryTable, ExchangerModal, ActivitiesModal, CreateMeal },
-  
-  created() {
-    this.ingredients.forEach(m => {
-      this.$store.commit("Plate/setProps", { ingredient: m, data: {
-        exchanger: this.getExchanger(m),
-        kcal: this.getKcal(m)
-      }});
-    });
+
+  mounted() {
+    this.syncAbsoluteBottom(-20, 50);
+  },
+  updated() {
+    this.syncAbsoluteBottom(-20, 50);
   },
 
-  mounted(){
-    this.syncAbsoluteBottom(-20, 50);
-  },
-  updated(){
-    this.syncAbsoluteBottom(-20, 50);
-  },
-  
   methods: {
     getExchanger(plateIngredient) {
       return this.getStoreIngredient(plateIngredient).exchanger * this.getPortions(plateIngredient);
@@ -76,7 +74,7 @@ export default {
     },
 
     getStoreIngredient(plateIngredient) {
-      return this.$store.getters['find'](plateIngredient.templateKey);
+      return this.$store.getters["find"](plateIngredient.templateKey);
     }
   },
   computed: {
@@ -96,7 +94,15 @@ export default {
         .map(key => this.ingredients[key].kcal)
         .reduce((previous, current) => previous + current);
     },
-    ...mapState("Plate", ["ingredients"])
+    ingredients() {
+      return this.$store.state.Plate.ingredients.map(m => {
+        return {
+          ...m,
+          exchanger: this.getExchanger(m),
+          kcal: this.getKcal(m),
+        };
+      });
+    }
   }
 };
 </script>
