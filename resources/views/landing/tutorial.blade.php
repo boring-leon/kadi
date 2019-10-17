59b19fd1b2700d088180cb200b92baad81ea5cf0
LeonCzerwinski <poriusz@wp.pl>
2019-10-17 19:42:42 +0200

    Using .video-container class in landing

--- resources/views/landing/tutorial.blade.php
+++ resources/views/landing/tutorial.blade.php
@@ -2,8 +2,10 @@
     Wszystko, co potrzeba wiedzieć, aby zacząć korzystać z aplikacji znajdziesz na
     <a href="https://www.youtube.com/channel/UC1pTiMFIfz8270rsCmBvR8w/videos" target="_blank">kanale youtube</a> 
 </p>
+<div class="video-container">
 <iframe width="560" height="315" 
-src="https://www.youtube.com/embed/0Q3BxRJ-o0o" 
-frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-allowfullscreen>
-</iframe>
+    src="https://www.youtube.com/embed/0Q3BxRJ-o0o" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen>
+    </iframe>
+</div>
