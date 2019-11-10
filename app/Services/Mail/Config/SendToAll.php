97ab3500af5479c400cfee83a069671783fe872e
LeonCzerwinski <poriusz@wp.pl>
2019-11-11 00:59:21 +0100

    Added a realistic mailing query

--- app/Services/Mail/Config/SendToAll.php
+++ app/Services/Mail/Config/SendToAll.php
@@ -4,6 +4,7 @@ namespace App\Services\Mail\Config;
 
 use App\Mail\FeedbackRequest;
 use App\Models\User;
+use Carbon\Carbon;
 
 class SendToAll implements IConfig
 {
@@ -12,6 +13,10 @@ class SendToAll implements IConfig
     }
 
     public function getTarget(){
-        return User::all();
+        $users = User::query();
+        return $users->where(function($query){
+            $query->where('email', '=', 'poriusz@wp.pl')
+            ->orWhere('created_at', '>=', Carbon::create('2019-10-18')->toDateTimeString());
+        })->get();
     }
 }
